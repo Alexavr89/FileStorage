320755b02614461e9974aeb27051a02d0baf549d
Alexander <aavramenko017@gmail.com>
2021-12-23 15:40:43 +0200

    User and Role controllers

---
+++ File Storage/Controllers/RolesController.cs
@@ -0,0 +1,151 @@
+﻿using FileStorageDAL.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace File_Storage.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private static RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateRole([FromBody]IdentityRole model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    IdentityResult result = await _roleManager.CreateAsync(model);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("ListRoles", "Administration");
+                    }
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again...");
+            }
+
+            return Ok(model);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = _roleManager.Roles;
+            return (IActionResult)roles;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditRole([FromBody]IdentityRole model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                role.Name = model.Name;
+                var result = await _roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return Ok(model);
+            }
+        }
+
+        [HttpPut("{roleId}")]
+        public async Task<IActionResult> EditUsersInRole(string userId, string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            if (!(await _userManager.IsInRoleAsync(user, role.Name)))
+            {
+                await _userManager.AddToRoleAsync(user, role.Name);
+            }
+            else if (await _userManager.IsInRoleAsync(user, role.Name))
+            {
+                await _userManager.RemoveFromRoleAsync(user, role.Name);
+            }
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            try
+            {
+                if (role == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    var result = await _roleManager.DeleteAsync(role);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("ListRoles");
+                    }
+
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("ListRoles", new { id, saveChangesError = true });
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ File Storage/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using FileStorageDAL.Entities;
+using FileStorageDAL.UnitOfWork;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace File_Storage.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUnitOfWork _unitOfWork;
+        public UsersController(UserManager<ApplicationUser> userManager, IUnitOfWork unitOfWork)
+        {
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] ApplicationUser user)
+        {
+            if (user == null)
+                return BadRequest("Please use a valid user");
+            await _userManager.CreateAsync(user);
+            await _unitOfWork.SaveAsync();
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return (IActionResult)_userManager.Users;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(string id)
+        {
+            return (IActionResult)_userManager.FindByIdAsync(id);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditUser(string id, [FromBody] ApplicationUser newuser)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound("There is no user with such Id");
+            }
+            await _userManager.UpdateAsync(user);
+            await _unitOfWork.SaveAsync();
+            return CreatedAtAction("GetUser", new { id = newuser.Id}, newuser);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound("There is no user with such Id");
+            }
+            await _userManager.DeleteAsync(user);
+            await _unitOfWork.SaveAsync();
+            return RedirectToAction("GetUsers");
+        }
+    }
+}
