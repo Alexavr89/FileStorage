23ec6b7471f766721457dbfb68049c200e3e07ca
Alexander <aavramenko017@gmail.com>
2021-12-28 20:59:23 +0200

    UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AdministrationComponent } from './administration/administration.component';
+import { FilesComponent } from './files/files.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    AdministrationComponent,
+    FilesComponent
   ],
   imports: [
     BrowserModule,
